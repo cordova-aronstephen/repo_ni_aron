9917657fc2af492c09824625a4d7e1ece217c76f
cordova-aronstephen <aronstephenscordova@gmail.com>
2025-12-04 00:26:15 +0800

    Add initial structure

---
+++ main_menu.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void display_hello();
+
+int main() {
+
+    display_hello();
+
+    return 0;
+}
+
+void display_hello() {
+    cout << "Welcome" << endl;
+}
